30d61fd19443963fa5e545b569fe0bf24fdb1b17
Black Kaiser <khairin13chan@gmail.com>
2024-09-02 16:16:14 +0800

    fix can access filament error

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 
 use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -52,7 +53,7 @@ class User extends Authenticatable implements FilamentUser
         return $this->hasMany(Institution::class);
     }
 
-    public function canAccessFilament(): bool
+    public function canAccessPanel(Panel $panel): bool
     {
         return true;
     }
